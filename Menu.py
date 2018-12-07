bb265791b63078e00b31b77da20981e7aa33df84
Dan Swift <dswift88@gmail.com>
2018-12-07 00:32:56 -0800

    Added Main Menu

--- Menu.py
+++ Menu.py
@@ -1,12 +1,17 @@
-class Menu(list):
-    def Header():
+class Menu:
+    def Header(header):
+        print('-' * 35)
+        print(' ' * (17 - int(len(header)/2)) + header)
+        print('-' * 35)
+
+    def StatusMessage():
         pass
 
-    def InfoMessage():
-        pass
-
-    def Body():
-        pass
+    def Options(menu_options, is_Main=False):
+        for count, item in enumerate(menu_options, start=1):
+            print(' [{}] '.format(count) + item)
+        if not is_Main:
+            print('Return to Previous Menu')
 
     def Prompt():
         pass
--- work_log.py
+++ work_log.py
@@ -1,5 +1,17 @@
 from Entry import db, Entry
+from Menu import Menu
+
+
+def main_menu():
+    menu_options = ['Add Entry', 'Search Entries']
+    while True:
+        Menu.Header("Main Menu")
+        Menu.StatusMessage()
+        Menu.Options(menu_options, is_Main=True)
+        input()
+
 
 if __name__ == "__main__":
     db.connect()
-    db.create_tables([Entry])
+    db.create_tables([Entry], safe=True)
+    main_menu()
